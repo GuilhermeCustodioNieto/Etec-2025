175b9ba67fac76f0558cfc63a4c787dc7875fbf4
Guilherme Custodio Nieto <GuilhermeCustodioNieto@gmail.com>
2025-02-21 08:19:30 -0300

    PAM - Aula 01 - Básico de react

---
+++ PAM/Aula 1/aula2/App.js
@@ -0,0 +1,38 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text >Guilherme</Text>
+      <Text style={styles.tituloTexto}>hello, world</Text>
+
+      <View>
+        <Image
+        style={styles.imagem}
+        source={{uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSs731nqGflTclgoWmkvwzkzqmOXJ_j3emixw&s'}}>
+
+        </Image>
+      </View>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  tituloTexto: {
+    color: '#f00',
+    fontSize: 20
+  },
+    imagem:{
+        width:500,
+        height:500
+      },
+    
+});
