25d50b07548a2b77a8f85413c0c4535f7c14c0b7
Guilherme Custódio Nieto <guilhermecustodionieto@gmail.com>
2025-04-13 17:51:41 -0300

    PW - Página principal do site de campeonato finalizada

--- PW/Inicio/Campeonato Paulista/front-end/src/pages/TablePage/TablePage.jsx
+++ PW/Inicio/Campeonato Paulista/front-end/src/pages/TablePage/TablePage.jsx
@@ -1,17 +1,27 @@
 import React from "react";
+import Header from "../../components/Header/Header";
+import Banner from "../../components/Banner/Banner";
 import FirstFase from "../../components/FirstFase/FirstFase";
+import QuarterFinals from "../../components/QuarterFinals/QuarterFinals";
+//import FirstFase from "../../components/FirstFase/FirstFase";
 import styles from "./TablePage.module.css";
+import Semifinal from "../../components/Semifinal/Semifinal";
+import Final from "../../components/Final/Final";
+import Footer from "../../components/Footer/Footer";
+import Table from "../../services/Table.json";
 
 function TablePage() {
   return (
     <div>
-      <header className={styles.header}>
-        <h1>Campeonato Paulista</h1>
-      </header>
-
-      <div>
-        <FirstFase></FirstFase>
+      <Header></Header>
+      <Banner></Banner>
+      <div className={styles.tables}>
+        <FirstFase data={Table["primeira-fase"]}></FirstFase>
+        <QuarterFinals></QuarterFinals>
+        <Semifinal></Semifinal>
+        <Final></Final>
       </div>
+      <Footer></Footer>
     </div>
   );
 }
